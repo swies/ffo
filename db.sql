a54c1ff1730cd444f3d519e66e81949e2934a0f7
Patrick Swieskowski <pat@swieskowski.net>
2010-07-04 15:16:41 -0500

    latest database structure

--- db.sql
+++ db.sql
@@ -1,45 +1,106 @@
-create database ffo default character set = 'utf8';
-use ffo;
+-- MySQL dump 10.13  Distrib 5.1.42, for apple-darwin10.2.0 (i386)
+--
+-- Host: localhost    Database: ffo
+-- ------------------------------------------------------
+-- Server version	5.1.42
 
-create table users (
-    id int auto_increment primary key,
-    lower_name varchar(250) unique,
-    password varchar(250),
-    display_name varchar(250),
-    claimed_at datetime,
-    claimed_by_ip varchar(15)
-) engine='innoDB', character set = 'utf8';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table posts (
-    id int auto_increment primary key,
-    linkhash varchar(250),
-    author varchar(250),
-    title text,
-    link text,
-    summary text,
-    tags text,
-    posted_at datetime,
-    checks integer default 0,
-    comments integer default 0,
-    key (linkhash)
-) engine='innoDB', character set = 'utf8';
+--
+-- Table structure for table `checks`
+--
 
-create table comments (
-    id int auto_increment primary key,
-    post_id integer,
-    user_id integer,
-    comment text,
-    created_at datetime,
-    key (post_id),
-    foreign key (post_id) references posts(id),
-    foreign key (user_id) references users(id)
-) engine='innoDB', character set = 'utf8';
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `checks` (
+  `post_id` int(11) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`post_id`,`user_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `checks_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  CONSTRAINT `checks_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table checks (
-    post_id integer,
-    user_id integer,
-    created_at datetime,
-    primary key (post_id, user_id),
-    foreign key (post_id) references posts(id),
-    foreign key (user_id) references users(id)
-) engine='innoDB', character set = 'utf8';
+--
+-- Table structure for table `comments`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `comment` text,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `post_id` (`post_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `posts`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `linkhash` varchar(250) DEFAULT NULL,
+  `author` varchar(250) DEFAULT NULL,
+  `title` text,
+  `link` text,
+  `summary` text,
+  `tags` text,
+  `posted_at` datetime DEFAULT NULL,
+  `checks` int(11) DEFAULT '0',
+  `comments` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `linkhash` (`linkhash`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lower_name` varchar(250) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
+  `display_name` varchar(250) DEFAULT NULL,
+  `claimed_at` datetime DEFAULT NULL,
+  `claimed_by_ip` varchar(15) DEFAULT NULL,
+  `ff2_password` varchar(32) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `lower_name` (`lower_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-07-04 15:15:29
