dafa18d7c23226a55fcfdfc2c795c46930e4b854
Patrick Swieskowski <pat@swieskowski.net>
2010-07-18 15:35:41 -0500

    embedly support

--- uimodules.py
+++ uimodules.py
@@ -1,6 +1,8 @@
 import tornado.web
+import embedly
 
 class Post(tornado.web.UIModule):
     def render(self, p, comment_link=True):
         return self.render_string("module-post.html", p=p,
+                                  embedly=embedly.embedly_re.match(p.link),
                                   comment_link=comment_link)
